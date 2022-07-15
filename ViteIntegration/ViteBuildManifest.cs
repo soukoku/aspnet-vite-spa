4fa3467eb3a4d264076e10efe33a0e60047d03f9
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2022-07-15 18:46:38 -0400

    Added module file resolution routine.

--- ViteIntegration/ViteBuildManifest.cs
+++ ViteIntegration/ViteBuildManifest.cs
@@ -45,5 +45,52 @@ namespace Soukoku.AspNetCore.ViteIntegration
             }
             return value ?? new Dictionary<string, ViteFileChunk>();
         }
+
+        /// <summary>
+        /// Resolves all files related to an entry chunk.
+        /// </summary>
+        /// <param name="chunkKey">chunk file key in the manifest.</param>
+        /// <param name="pathBase">Base url path to use.</param>
+        /// <returns></returns>
+        public ResolvedFiles ResolveEntryChunk(string chunkKey, string pathBase = "/")
+        {
+            var resolved = new ResolvedFiles();
+            if (Current.TryGetValue(chunkKey, out ViteFileChunk? rootChunk) &&
+                rootChunk != null && rootChunk.IsEntry)
+            {
+                resolved.MainModule = pathBase + rootChunk.File;
+                if (rootChunk.Css != null)
+                {
+                    resolved.CssFiles.AddRange(rootChunk.Css.Select(path => pathBase + path));
+                }
+                if (rootChunk.DynamicImports != null)
+                {
+                    foreach (var subKey in rootChunk.DynamicImports)
+                    {
+                        PopulateSubChunk(resolved, subKey, pathBase);
+                    }
+                }
+
+                resolved.CssFiles = resolved.CssFiles.Distinct().ToList();
+            }
+            return resolved;
+        }
+
+        private void PopulateSubChunk(ResolvedFiles resolved, string chunkKey, string pathBase)
+        {
+            var chunk = Current[chunkKey];
+            resolved.PreloadModules.Add(pathBase + chunk.File);
+            if (chunk.Css != null)
+            {
+                resolved.CssFiles.AddRange(chunk.Css.Select(path => pathBase + path));
+            }
+            if (chunk.DynamicImports != null)
+            {
+                foreach (var subKey in chunk.DynamicImports)
+                {
+                    PopulateSubChunk(resolved, subKey, pathBase);
+                }
+            }
+        }
     }
 }
