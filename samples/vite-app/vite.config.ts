30381d6274120bd125e443489087bbf5d8fde0f9
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2025-06-03 21:03:47 -0400

    Update to latest vue/vite.

--- samples/vite-app/vite.config.ts
+++ samples/vite-app/vite.config.ts
@@ -2,55 +2,50 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 import fs from 'fs'
 import path from 'path'
 import child_process from 'child_process'
-import pkg from './package.json' assert { type: 'json' }
+import { env } from 'process'
+import pkg from './package.json' with { type: 'json' }
 
-const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-    ? `${process.env.APPDATA}/ASP.NET/https`
-    : `${process.env.HOME}/.aspnet/https`
-
-const certificateName = pkg.name || 'vueapp.client'
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
 const isDev = process.env.NODE_ENV === 'development'
 
-if (isDev && (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath))) {
-  if (
-    0 !==
-    child_process.spawnSync(
-      'dotnet',
-      [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-        '--trust'
-      ],
-      { stdio: 'inherit' }
-    ).status
-  ) {
-    throw new Error('Could not create certificate.')
+let keyFilePath = ''
+let certFilePath = ''
+if (isDev) {
+  const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+      ? `${env.APPDATA}/ASP.NET/https`
+      : `${env.HOME}/.aspnet/https`
+
+  const certificateName = pkg.name || 'appname'
+  certFilePath = path.join(baseFolder, `${certificateName}.pem`)
+  keyFilePath = path.join(baseFolder, `${certificateName}.key`)
+
+  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    fs.mkdirSync(baseFolder, { recursive: true })
+    if (
+      0 !==
+      child_process.spawnSync(
+        'dotnet',
+        ['dev-certs', 'https', '--export-path', certFilePath, '--format', 'Pem', '--no-password', '--trust'],
+        { stdio: 'inherit' }
+      ).status
+    ) {
+      throw new Error('Could not create certificate.')
+    }
   }
 }
 
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   server: {
     https: isDev
       ? {
@@ -60,6 +55,11 @@ export default defineConfig({
       : undefined,
     port:3000,
     hmr: { host: 'localhost', clientPort: 3000 }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
   },
   build: {
     // outDir: '../wwwroot',
